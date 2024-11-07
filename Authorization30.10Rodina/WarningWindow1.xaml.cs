6586d368c7e0f963e5be26ad5c6d875c6e7cd77d
Val200ya <australia636@gmail.com>
2024-11-07 10:24:03 +0300

    Created some windows and pages

---
+++ Authorization30.10Rodina/WarningWindow1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Authorization30._10Rodina
+{
+    /// <summary>
+    /// Логика взаимодействия для WarningWindow1.xaml
+    /// </summary>
+    public partial class WarningWindow1 : Window
+    {
+        public WarningWindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
